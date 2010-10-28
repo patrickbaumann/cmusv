db837fd33ee3a04d7df531f3bda2a5db5b0347cd
patrickbaumann <baumannpat@gmail.com>
2010-10-27 17:50:11 -0700

    adding deliverable_test file

---
+++ test/unit/deliverable_test.rb
@@ -0,0 +1,99 @@
+require 'test_helper'
+
+class DeliverableTest < ActiveSupport::TestCase
+
+  def test_valid_attributes
+    d = Deliverable.new
+    assert !(d.save), "should not save without required attributes"
+    
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = 1
+    d.individual = true
+           
+    assert d.save, "should save with required fields"
+
+    d.comments = "testing testing testing"
+    d.task_number = 1
+    
+    assert d.save, "should still save with optional fields"
+  end
+
+  def test_team_upload
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = teams(:one).id
+    d.individual = false
+    assert d.save, "Should save with team information"
+    assert_equal d.team.name, "Team Alpha", "Should be classified as a team upload"
+    assert !d.individual?, "Should save as an individual deliverable"  
+  end
+ 
+  def test_individual_upload
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.individual = true
+    
+    assert d.save, "Should save with team information"
+    assert d.individual?, "Should save as an individual deliverable"  
+  end
+
+  def test_reupload_versioning
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = teams(:one).id
+    d.individual = true
+    
+    d.save
+    
+    d.student_file_file_name = "test2"
+    d.save
+    
+    assert_equal 2, d.versions, "should have two versions after second save"
+  end
+  
+  def test_faculty_file_view
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = teams(:teamOne)
+    d.individual = true
+    d.save
+    
+    assert d.canView?(users(:Faculty_Francine)), "team faculty should be able to view with user object passed"
+    assert d.canView?(2), "team faculty (2) should be able to view with id passed"
+  end
+
+  def test_faculty_file_response_upload
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = 1
+    d.individual = true
+    assert d.save, "should save with required fields"
+    
+    d.faculty_file_file_name = "test"
+    assert d.save, "should save with factulty file fields"
+  end
+  
+  def test_file_permissions
+    d = Deliverable.new
+    d.person_id = 1
+    d.student_file_file_name = "test"
+    d.team_id = teams(:teamOne)
+    d.individual = true
+    d.save
+    assert d.canView?(1), "uploader can view file"
+    assert !d.canView?(2), "uploader can view file"
+      
+    d.individual = false
+    d.save
+    assert d.canView?(1), "uploader can view file"
+    assert d.canView?(2), "team member can view file"
+    assert !d.canView?(2), "non team member can view file"
+  end
+    
+end
